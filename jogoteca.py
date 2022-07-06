89454c92b0dcfa8d41c5126b20e660f5c2671d15
Kevin <kevinmgsd@gmail.com>
2022-07-06 12:09:16 -0300

    k

---
+++ jogoteca.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+class Jogos:
+    def __init__(self,nome,categoria,console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+a1 = Jogos("God of War","Hack n Slash","PS2")
+a2 = Jogos("Lol","MOBA","PC")
+a3 = Jogos("Batman","Ação e Aventura","PC/PS4/XBOX")
+a4 = Jogos("CS:GO","FPS","PC")
+
+lista = [a1,a2,a3,a4]
+
+@app.route('/inicio')
+def ola():
+    return render_template('lista.html',titulo='Jogos',jogos= lista)
+
+app.run()
